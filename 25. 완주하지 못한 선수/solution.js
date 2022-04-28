2bf515a8f60f0b1c2b351a8a135ac89b74d992b1
youahleum <dkfma6033@naver.com>
2022-04-28 16:07:11 +0900

    완주하지 못한 선수 -다른 풀이

--- 25. 완주하지 못한 선수/solution.js
+++ 25. 완주하지 못한 선수/solution.js
@@ -1,4 +1,4 @@
-//시간초과
+//시간초과 효율성테스트 탈락
 function solution(participant, completion) {
   // let arr1=participant
   // let arr2=completion
@@ -7,4 +7,15 @@ function solution(participant, completion) {
     participant.splice(ind, 1);
   });
   return participant.join("");
+}
+
+// 성공한 문제풀이
+function solution(participant, completion) {
+  let parArr = participant.sort();
+  let compArr = completion.sort();
+  for (i = 0; i < parArr.length; i++) {
+    if (parArr[i] !== compArr[i]) {
+      return parArr[i];
+    }
+  }
 }
