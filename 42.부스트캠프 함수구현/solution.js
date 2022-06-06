88e89c77102a8c0f01ed0b7d8485215837fc95ec
youahleum <dkfma6033@naver.com>
2022-06-07 04:32:29 +0900

    부스트캠프 함수문제  완료

--- 42.부스트캠프 함수구현/solution.js
+++ 42.부스트캠프 함수구현/solution.js
@@ -1,27 +1,27 @@
-function solution(arr) {
-  arr.sort((a, b) => {
-    a - b;
-  });
+let arr = [1, 2, 3, 3, 3, 3, 4, 4].sort((a, b) => a - b);
+// console.log("a");
+// console.log(arr);
+function solution() {
+  // arr.sort((a, b) => a - b);
+  let arr = [3, 2, 4, 4, 2, 5, 2, 5, 5].sort((a, b) => a - b);
   let count = 0;
-  let arr = [];
-  // for(i of arr){
-  //   if(i===arr[0]){
-  //     count+=1
-  //   }
-
-  // }
-  // let a = arr[0];
-  // while(!arr.include(a)){
-  //   if(arr[0]===a){
-  //     count++
-  //     arr.shift()
-  //   }
-
-  // }
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i - 1] === arr[0]) {
+  console.log(count);
+  let resultArr = [];
+  // console.log(arr);
+  while (arr.length > 0) {
+    let a = arr[0];
+    if (a === arr[1]) {
       count++;
+      arr.shift();
     } else {
+      arr.shift();
+      if (count > 0) {
+        resultArr.push(count + 1);
+        count = 0;
+      }
     }
   }
+  console.log(resultArr);
+  // console.log(arr);
 }
+solution();
