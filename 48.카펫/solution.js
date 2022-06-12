191dd31296da1a1961985fb3453f842cc9af87f3
youahleum <dkfma6033@naver.com>
2022-06-13 00:37:37 +0900

    카펫 edit

--- 48.카펫/solution.js
+++ 48.카펫/solution.js
@@ -13,3 +13,15 @@ function solution(brown, yellow) {
   // var answer = [];
   return answer;
 }
+//------------------
+function solution2(brown, yellow) {
+  for (i = 1; i <= yellow; i++) {
+    if (yellow % i === 0) {
+      let x = yellow / i;
+      if (2 * (x + 2) + 2 * (i + 2) - 4 === brown) {
+        return [x + 2, i + 2];
+        // 위처럼 array에 넣고 밖에서 return 하는것보다 훨씬 속도가 빠르다
+      }
+    }
+  }
+}
