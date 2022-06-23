514b9b7f4b7bb23968a01a1f2909fd4f4bba45f4
youahleum <dkfma6033@naver.com>
2022-06-24 01:51:47 +0900

    소수찾기

--- 03.소수찾기/solution.js
+++ 03.소수찾기/solution.js
@@ -20,3 +20,19 @@
 //     var answer = arr.length;
 //     return answer;
 // }
+
+function solution(n) {
+  let arr = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+
+  return arr.filter((e) => e).length;
+}
