df12b09935155d7cbdd9920c34f7d5756318f161
youahleum <dkfma6033@naver.com>
2022-06-26 03:51:00 +0900

    다음 큰 숫자

--- 53.다음 큰 숫자/solution.js
+++ 53.다음 큰 숫자/solution.js
@@ -1,14 +1,26 @@
 function solution(n) {
   var answer = "";
   // return answer;
+  let m = n;
   let num = n.toString(2);
   let oneLength = num.match(/1/g).length;
-  console.log(oneLength);
-  console.log(num);
+  // console.log(oneLength)
+  // console.log(num)
   if (num.includes("0")) {
-    while (true) {}
+    while (true) {
+      m += 1;
+      let testNum = m.toString(2);
+      // console.log(testNum)
+      if (oneLength === testNum.match(/1/g).length) {
+        answer = testNum;
+        // console.log('answer',answer)
+        break;
+      }
+    }
   } else {
     answer = [num.slice(0, 1), 0, num.slice(1)].join("");
-    console.log(answer);
+    // console.log(answer)
   }
+  // console.log(parseInt(answer,2))
+  return parseInt(answer, 2);
 }
