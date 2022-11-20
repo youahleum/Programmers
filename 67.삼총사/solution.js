774f645df56008e3e88030a95fb94bc705a379c3
youahleum <dkfma6033@naver.com>
2022-11-20 16:02:01 +0900

    삼총사

---
+++ 67.삼총사/solution.js
@@ -0,0 +1,11 @@
+function solution(number) {
+  let answer = 0;
+  for (i = 0; i < number.length; i++) {
+    for (j = i + 1; j < number.length; j++) {
+      for (k = j + 1; k < number.length; k++) {
+        number[i] + number[j] + number[k] === 0 ? answer++ : null;
+      }
+    }
+  }
+  return answer;
+}
